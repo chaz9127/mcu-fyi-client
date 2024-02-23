c31da8c0f40588c7f0269f32b9a0d55f87763df1
CJ Douglas <cj@lola.com>
2024-02-22 21:23:58 -0500

    Removing port from production

--- src/constants/server.ts
+++ src/constants/server.ts
@@ -1,9 +1,9 @@
 const SERVER_PORT = (() => {
   switch(import.meta.env.NODE_ENV) {
     case 'development':
-      return '3001';  
+      return ':3001';  
     default:
-      return import.meta.env.PORT || '3001';
+      return '';
   }
 })();
 
@@ -12,9 +12,9 @@ const domain = import.meta.env.VITE_SERVER_URL || 'http://localhost';
 const SERVER_URL = (() => {
   switch(import.meta.env.NODE_ENV) {
     case 'development':
-      return `${domain}:${SERVER_PORT}`;  
+      return `${domain}${SERVER_PORT}`;  
     default:
-      return `${domain}:${SERVER_PORT}`;
+      return `${domain}${SERVER_PORT}`;
   }
 })();
 
