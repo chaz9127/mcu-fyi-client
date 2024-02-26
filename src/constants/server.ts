ac1ec3d1ab19b3d93c8cd7d17032262103b48118
CJ <chasanid@gmail.com>
2024-02-26 17:09:30 -0500

    removing server warnings

--- src/constants/server.ts
+++ src/constants/server.ts
@@ -1,5 +1,5 @@
 const SERVER_PORT = (() => {
-  switch(import.meta.env.NODE_ENV) {
+  switch(import.meta.env.VITE_NODE_ENV) {
     case 'development':
       return ':3001';  
     default:
@@ -10,7 +10,7 @@ const SERVER_PORT = (() => {
 const domain = import.meta.env.VITE_SERVER_URL || 'http://localhost';
 
 const SERVER_URL = (() => {
-  switch(import.meta.env.NODE_ENV) {
+  switch(import.meta.env.VITE_NODE_ENV) {
     case 'development':
       return `${domain}${SERVER_PORT}`;  
     default:
--- src/utils/api.tsx
+++ src/utils/api.tsx
@@ -1,15 +1,15 @@
 import { SERVER_URL } from '../constants/server.ts';
 import { APIMethod } from '../types';
 
-const callApi = async (url: string, params?: APIMethod) => {
+const callApi = async (directory: string, params?: APIMethod) => {
 
   const betterParams = {
     method: params?.method || 'get',
   }
   if (betterParams?.method === 'get') {
-    return await (await fetch(`${SERVER_URL}${url}`, betterParams)).json();
+    return await (await fetch(`${SERVER_URL}${directory}`, betterParams)).json();
   } else if (betterParams?.method === 'delete') {
-    return await (await fetch(`${SERVER_URL}${url}`, betterParams)).json();
+    return await (await fetch(`${SERVER_URL}${directory}`, betterParams)).json();
   }
 };
 
