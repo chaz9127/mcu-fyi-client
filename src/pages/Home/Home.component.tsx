5b8644c58ae111a8ab367ce1343c1e2404c0f84b
CJ <chasanid@gmail.com>
2024-04-23 13:47:56 -0400

    including sorting on home page

--- src/pages/Home/Home.component.tsx
+++ src/pages/Home/Home.component.tsx
@@ -8,6 +8,22 @@ import { Nav } from '../../components/Nav/Nav.component';
 
 export const Home = () => {
   const [mediaCollection, setMediaCollection] = useState<Array<MediaType>>([])
+  const [sortByValue, setSortByValue] = useState<string>('releaseDate');
+
+  const sortByCompare = (a, b) => {
+    if (sortByValue === 'name') {
+      if ( a[sortByValue] < b[sortByValue]) {
+        return -1;
+      } else if ( a[sortByValue] > b[sortByValue]) {
+        return 1;
+      } else {
+        return 0;
+      }
+    } else {
+      return a['releaseDate'] - b['releaseDate'];
+    }
+    
+  }
 
   useEffect(() => {
     const getMedia = async () => {
@@ -19,16 +35,31 @@ export const Home = () => {
     
   }, [])
 
+  useEffect(() => {
+    setMediaCollection([...mediaCollection].sort(sortByCompare));
+  }, [sortByValue])
+
+  const setSort = (e: React.FormEvent<HTMLInputElement>) => {
+    setSortByValue(e.target.value);
+  }
+
   return (
     <>
       <Nav />
       <div className="app-container">
         <Featured />
+        <div className="filter-bar">
+          <label>Sort By:</label>
+          <select className="select-menu" onChange={setSort}>
+            <option value='releaseDate'>Release Date</option>
+            <option value='name'>Title</option>
+          </select>
+        </div>
         <div className="app-content">
           {mediaCollection.map((media: MediaType, idx: number) => {
               return (
-                <div className="media-container">
-                  <Media media={media} key={idx}/>
+                <div className="media-container" key={idx}>
+                  <Media media={media}/>
                 </div>
               )
             })}
