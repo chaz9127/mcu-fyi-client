e36a063772efd63fbe1c100f89264f8a85601889
CJ <chasanid@gmail.com>
2024-05-07 21:57:45 -0400

    removing unused import

--- src/features/store.ts
+++ src/features/store.ts
@@ -1,13 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from '../api/apiSlice';
 import authReducer from './authSlice';
-
-import userReducer from '../features/usersSlice';
 import { setupListeners  } from "@reduxjs/toolkit/query";
 
 export const store = configureStore({
     reducer: {
-        users: userReducer,
         [apiSlice.reducerPath]: apiSlice.reducer,
         auth: authReducer,
     },
